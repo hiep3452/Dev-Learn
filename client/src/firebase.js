9906ec786472d3f475fbe0e46fc4e823d1ddf490
Anhthu02tranthi <133476331+Anhthu02tranthi@users.noreply.github.com>
2024-11-14 09:09:23 +0700

    update firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,13 +4,16 @@ import { getStorage } from "firebase/storage";
 
 // Cấu hình Firebase của ứng dụng web
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: "devb-blog.firebaseapp.com",
-    projectId: "devb-blog",
-    storageBucket: "devb-blog.appspot.com",
-    messagingSenderId: "26828132532",
-    appId: "1:26828132532:web:644df8d0c5cb5aa7aac7e1",
-};
+    apiKey:  import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: "devlearn-f2703.firebaseapp.com",
+    projectId: "devlearn-f2703",
+    storageBucket: "devlearn-f2703.firebasestorage.app",
+    databaseURL: "https://devlearn-f2703-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    messagingSenderId: "675420268590",
+    appId: "1:675420268590:web:31cc1203248c80f02157bf",
+    measurementId: "G-WFYWWD5LFD"
+  };
+  
 
 // Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
